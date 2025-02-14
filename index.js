21236cf678ef19c8f5adaafb2a7bf651a3873e2e
Jcrastreamento <jcrastreamentohorizonte@gmail.com>
2025-02-13 21:54:14 -0300

    Adicionando sistema de login

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Servindo o arquivo HTML
+// Servindo arquivos estáticos
+app.use(express.static(__dirname));
+
+// Redireciona para a página de login por padrão
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "login.html"));
 });
 
 app.listen(port, () => {
